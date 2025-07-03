435becdeb6c8d0cef6174029113b66a241d17130
akibagakuto <0mj0922w7260b6p@au.com>
2025-07-03 15:30:21 +0900

    jump idou

--- Assets/player/playerTest.cs
+++ Assets/player/playerTest.cs
@@ -1,29 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.VFX;
 
 public class playerTest : MonoBehaviour
 {
-    Rigidbody2D rd;
+
+    [SerializeField]
+    private int moveSpeed;
+    public Rigidbody2D rd;
     public float y;
+
+    private GamePlayerInput inputActions;
+
+    private bool isGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputActions = new GamePlayerInput();
+        inputActions.Enable();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
-            Debug.Log(rd.gameObject.name);
+            Debug.Log("rd.gameObject.name");
             Vector2 force = new Vector2(0, y * 10);
-            rd.AddForce(force);
+            rd.velocity = force;
+            isGround = false;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        var direction = inputActions.Player.Move.ReadValue<Vector2>();
+        direction.x *= moveSpeed;
+        direction.y = rd.velocity.y;
+        //moveSpeed
+        rd.velocity = direction;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
         {
-            if (Input.GetKey(KeyCode.D)) ;
+            isGround = true;
         }
     }
 }
