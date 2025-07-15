322a61dc5564a80779aa71a4fc8b43d74989262c
Chloevsiel <cmx1677145281@gmail.com>
2025-07-15 15:33:44 +0900

    Update

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -6,6 +6,8 @@ public class PlayerMove : MonoBehaviour
 {
     public float Horizontal;
     public float speed = 5;
+    public float runSpeed = 8f;
+    public KeyCode runKey = KeyCode.LeftShift;
     public float jumpingPower = 10;
     private bool isFacingRight = true;
 
@@ -20,8 +22,11 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            float currentSpeed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? runSpeed : speed;
+            float adjustedJumpPower = jumpingPower + (Mathf.Abs(Horizontal) * currentSpeed * 0.2f); // scale factor
+            rb.velocity = new Vector2(rb.velocity.x, adjustedJumpPower);
         }
+
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
@@ -31,7 +36,8 @@ public class PlayerMove : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(Horizontal * speed, rb.velocity.y);
+        float currentSpeed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? runSpeed : speed;
+        rb.velocity = new Vector2(Horizontal * currentSpeed, rb.velocity.y);
     }
     private bool IsGrounded()
     {
